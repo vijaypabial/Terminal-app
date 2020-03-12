b07b4b3311741d0c08b4ea60a404e3cf29afd070
vijaypabial <vijaypabial@yahoo.com>
2020-03-12 12:03:31 +1000

    Cleaned up the code structure and fixed issue of exit from the app. I need to addd more information to the menus, so I can have a better user interface

--- tries.rb
+++ tries.rb
@@ -21,4 +21,22 @@
 # p Dir.entries("./supplier1").select {|f| !File.directory? f}
 
 # newrow = row.each_with_index { |rowcontent , row_num| puts "#     {rowcontent} #{row_num}" }
-p Dir.entries("./supplier1").select {|f| !File.directory? f}
+# p Dir.entries("./supplier1").select {|f| !File.directory? f}
+require 'csv'
+
+# CSV.open('test.csv','w', 
+#     :write_headers=> true,
+#     :headers => ["numerator","denominator","calculation"] #< column header
+#   ) do|hdr|
+#   1.upto(12){|numerator|
+#     1.upto(12){ |denominator|
+#       data_out = [numerator, denominator, numerator/denominator.to_f]
+#       hdr << data_out
+#     }
+#   }
+# end
+# Adding new data
+headers = ["name","height", "age","gender"]
+CSV.open('file.csv', 'a+') do |row|
+ row << headers
+end
